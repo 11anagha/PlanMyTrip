9731456f84c88e5dd76c25b12093eb40ed483d67
Anagha Anil <11anaghaanil@gmail.com>
2025-02-26 02:28:48 +0530

    Pdf generation implemented

--- itinerary/urls.py
+++ itinerary/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import generate_itinerary, download_itinerary_pdf
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
-    path("generate_itinerary/", views.generate_itinerary, name="generate_itinerary")
+    path("generate_itinerary/", views.generate_itinerary, name="generate_itinerary"),
+    path("download-itinerary/", views.download_itinerary_pdf, name="download_itinerary"),
 ]

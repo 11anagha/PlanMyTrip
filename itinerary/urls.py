94bf7fe85741d847fbf42412ee35b58f5529d985
Anagha Anil <11anaghaanil@gmail.com>
2025-02-26 17:03:05 +0530

    Previously generated itineraries can be viewed and deleted

--- itinerary/urls.py
+++ itinerary/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 from .views import generate_itinerary, download_itinerary_pdf
+from .views import delete_itinerary
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path("generate_itinerary/", views.generate_itinerary, name="generate_itinerary"),
-    path("download-itinerary/", views.download_itinerary_pdf, name="download_itinerary"),
+    path("download_itinerary/", views.download_itinerary_pdf, name="download_itinerary"),
+    path("view-itinerary/<int:itinerary_id>/", views.view_itinerary, name="view_itinerary"),
+    path("delete_itinerary/<int:itinerary_id>/", delete_itinerary, name="delete_itinerary"),
 ]
